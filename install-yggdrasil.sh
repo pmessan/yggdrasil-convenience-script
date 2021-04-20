8e6af2423f0acb5c2311333170a051c96554dbc1
Peter-Newman Messan <p.messan@jacobs-university.de>
2021-04-20 05:32:44 +0200

    Update install-yggdrasil.sh

--- install-yggdrasil.sh
+++ install-yggdrasil.sh
@@ -8,20 +8,20 @@
 ################################################################
 
 # install apt key mgt pkgs
-
 apt-get install dirmngr gnupg
 
 # add apt keys
 gpg --fetch-keys https://neilalexander.s3.dualstack.eu-west-2.amazonaws.com/deb/key.txt
 gpg --export 569130E8CA20FBC4CB3FDE555898470A764B32C9 | apt-key add -
 
-
+# add apt source
 echo 'deb http://neilalexander.s3.dualstack.eu-west-2.amazonaws.com/deb/ debian yggdrasil' | tee /etc/apt/sources.list.d/yggdrasil.list
 
 # install
 apt-get update
 apt-get install -y yggdrasil
 
+# enable yggdrasil run on startup
 systemctl start yggdrasil
 systemctl enable yggdrasil
 
@@ -37,6 +37,7 @@ yggdrasilctl getself | grep address | xargs
 # store ygg:ipv6:addr + hostname-ygg in /etc/hosts
 echo "$(yggdrasil -address -useconffile /etc/yggdrasil.conf) $(echo $HOSTNAME | awk '{split($0,a,"."); print a[1]}')-ygg" >> /etc/hosts
 
+#finish
 echo "Yggdrasil ipv6 address stored as hostname $(echo $HOSTNAME | awk '{split($0,a,"."); print a[1]}')-ygg in /etc/hosts.
 You can change this should you wish.
 
